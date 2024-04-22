05a99a7ed3d458b580fdf15c6f50f1e6f7dd38a3
Hilton Sousa <hilton.sousa@gmail.com>
2024-04-22 13:28:06 -0300

    Minor changes

--- challenge.py
+++ challenge.py
@@ -4,8 +4,8 @@ import math
 def get_histogram(s):
     return {c:sum([1 if b == c else 0 for b in s]) for c in s}  
     
-searched = 'hilton'
-bowl = list('hP.E.N.Y.M.A.C.N.iltoxhiltonPENNYMACPENNYMACCAMYNNEPhiltonnotlihn')
+searched = 'PENNYMAC'
+bowl = 'hP.E.N.Y.M.A.C.N.iltoxhYiltonPENNMACPENMMNYMACCAYNNEPhiltonnotlihn'
 
 histo_searched = get_histogram(searched)
 histo_bowl = get_histogram(bowl)
@@ -16,12 +16,12 @@ print(histo_bowl)
 n_found = 0
 # the minimum: all chars from the searched term must be in the bowl
 if set(histo_searched.keys()).issubset(set(histo_bowl.keys())):
-    # ok, all searched chars are in the bowl. we then evaluate the amount of times each one of them occurs,
-    # compared to the amount of times each one occurs in the searched term. in order to obtain the complete
-    # search term, we take the smallest result of this quotient
-    n_found = math.floor(
-        min(
-            [histo_bowl[v]/histo_searched[v] for v in histo_searched.keys()]
+    # ok, all searched chars are in the bowl! we then evaluate the amount of times each one of them occurs,
+    # compared to the amount of times each one occurs in the searched term. this way we'll have a list
+    # of how many times each of the chars of the search term present in the bowl can be used to build the search term 
+    # - and then we take the smallest of these (i.e., the number of how many times the searched term can be built from the bowl chars)
+    n_found = min(
+            [histo_bowl[v]//histo_searched[v] for v in histo_searched.keys()]
         )
-    )
+
 print(n_found)
